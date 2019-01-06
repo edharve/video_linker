94dc70b00257c4a46ddea72e1e4462d529f47a48
edharve <edharve@gmail.com>
2019-01-06 13:56:07 +0000

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+    
+    def new
+    end
+    
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+    
+    
+def create
+  @article = Article.new(article_params)
+ 
+  @article.save
+  redirect_to @article
+end
+    
+ def youtube_id
+     
+     @article.title = @song
+     
+    YouTubeAddy.extract_video_id(@song.url)
+  end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+    
+    
+    
+end
---
+++ test/controllers/artciles_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArtcilesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
